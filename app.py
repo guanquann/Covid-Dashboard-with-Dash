d5efece2d8a94d04110e0683b2ddc7e4b36dcf55
spartan737 <tanguanquan@yahoo.com>
2021-01-10 16:24:52 +0800

    first upload

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_table
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, State
+from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
