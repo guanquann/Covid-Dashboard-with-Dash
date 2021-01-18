b94c126ca82ef309633d78dca9d5b465d04d2437
spartan737 <tanguanquan@yahoo.com>
2021-01-18 22:31:15 +0800

    testing

--- app.py
+++ app.py
@@ -175,5 +175,5 @@ def update_output(date):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
-    # app.run_server(debug=False,  port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
+    # app.run_server(debug=True)
+    app.run_server(debug=False,  port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
